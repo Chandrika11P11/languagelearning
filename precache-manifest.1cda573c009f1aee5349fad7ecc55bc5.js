3077cea66b57faa3615948b20529d83a899214eb
Chandrika11P11 <94225366+Chandrika11P11@users.noreply.github.com>
2024-01-31 14:08:32 +0530

    Updates

---
+++ precache-manifest.1cda573c009f1aee5349fad7ecc55bc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5fde4612517ffb6d0f9a4caec36babc",
+    "url": "/languagelearning/index.html"
+  },
+  {
+    "revision": "5fcccc55379fadc885ac",
+    "url": "/languagelearning/static/css/main.20b43c59.chunk.css"
+  },
+  {
+    "revision": "ee1b11a874cee6860cb4",
+    "url": "/languagelearning/static/js/2.912c6ad4.chunk.js"
+  },
+  {
+    "revision": "5fcccc55379fadc885ac",
+    "url": "/languagelearning/static/js/main.628b2aaa.chunk.js"
+  },
+  {
+    "revision": "3a6f51359499be0d531e",
+    "url": "/languagelearning/static/js/runtime~main.21b45e12.js"
+  },
+  {
+    "revision": "d1b5b521ccc07812f6774dd79a4d06e3",
+    "url": "/languagelearning/static/media/left.d1b5b521.png"
+  },
+  {
+    "revision": "6cf6e878c85c2fe38c5760f987036aa4",
+    "url": "/languagelearning/static/media/right.6cf6e878.png"
+  }
+]);
